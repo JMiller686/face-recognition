2a00289cae2a5196240311b415f56d78997dec12
JMiller686 <jmiller686@gmail.com>
2019-02-27 15:41:52 -0600

    added honeycomb background to ImageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
 				{`This magic brain will detect faces in your pictures.  Enter a url to try it out.`}
 			</p>
 			<div className="center">
-				<div className="center pa4 br3 shadow-5">
+				<div className="form center pa4 br3 shadow-5">
 					<input type="text" className="f4 pa2 w-70 center"/>
 					<button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
 				</div>
