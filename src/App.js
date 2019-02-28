f0b0dd1eb0ffd92cf604fc2a1be3b15302ec0035
JMiller686 <jmiller686@gmail.com>
2019-02-28 11:27:00 -0600

    finalized desired settings for the particles

--- src/App.js
+++ src/App.js
@@ -11,39 +11,99 @@ import 'tachyons';
 const particlesOptions = {
   particles: {
     number: {
-      value: 100,
+      value: 145,
       density: {
-        enable: true,
-        value_area: 1000
+        enable: false,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#ffffff'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
       }
     },
     line_linked: {
-      shadow: {
-        enable: true, 
-        color: "#3CA9D1",
-        blur: 5
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 4,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
       }
     }
   },
   interactivity: {
-    detect_on: "window",
+    detect_on: 'window',
     events: {
       onhover: {
-        enable:true, 
-        mode: "grab"
+        enable: true,
+        mode: 'grab'
       },
-      resize:true
+      onclick: {
+        enable: true,
+        mode: 'push'
+      },
+      resize: true
     },
     modes: {
       grab: {
-        distance:227.77222777222775,
+        distance: 200,
         line_linked: {
-          opacity:1
+          opacity: 1
         }
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4
       }
     }
   },
-  retina_detect:true
+  retina_detect: true
 }
 
 class App extends Component {
