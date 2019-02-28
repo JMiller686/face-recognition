fc9064c96039f86150185fe0380f5288ced46548
JMiller686 <jmiller686@gmail.com>
2019-02-28 11:09:56 -0600

    added particles animation to background of App and configured the settings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
-import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+import './App.css';
 import 'tachyons';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true, 
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable:true, 
+        mode: "grab"
+      },
+      resize:true
+    },
+    modes: {
+      grab: {
+        distance:227.77222777222775,
+        line_linked: {
+          opacity:1
+        }
+      }
+    }
+  },
+  retina_detect:true
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className="particles"
+          params={particlesOptions}
+        />
         <Navigation/>
         <Logo />
         <Rank />
