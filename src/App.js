5bcf187c36b4bb24bde775bc5463ddaaa075fc20
JMiller686 <jmiller686@gmail.com>
2019-03-04 09:47:03 -0600

    added a fetch to App.js to connect to the node server

--- src/App.js
+++ src/App.js
@@ -126,6 +126,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/')
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('face-image');
